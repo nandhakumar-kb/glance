4127856b4be4c1a2a3a96f7477b14ff9dbefbbaa
nandhakumar-kb <nandhakumarkb2005@gmail.com>
2025-12-20 18:46:42 +0530

    Update all 50 affiliate links with correct India and USA URLs and bump version to 1.1

--- src/ProductData.js
+++ src/ProductData.js
@@ -4,47 +4,47 @@ export const products = [
     title: "Atomic Habits",
     author: "James Clear",
     image: "https://covers.openlibrary.org/b/isbn/9780735211292-L.jpg",
-    affiliateLinkIN: "https://amzn.to/4gjA7Lf",
-    affiliateLinkUS: "https://amzn.to/48Hmvq9"
+    affiliateLinkIN: "https://amzn.to/4qjA7Lf",
+    affiliateLinkUS: "https://amzn.to/48Hmgs9"
   },
   {
     id: 2,
     title: "The Psychology of Money",
     author: "Morgan Housel",
     image: "https://covers.openlibrary.org/b/isbn/9780857197689-L.jpg",
-    affiliateLinkIN: "https://amzn.to/3KMY8xl",
-    affiliateLinkUS: "https://amzn.to/4ge54Ar"
+    affiliateLinkIN: "https://amzn.to/3KMYBxl",
+    affiliateLinkUS: "https://amzn.to/4qe54Ar"
   },
   {
     id: 3,
     title: "Deep Work",
     author: "Cal Newport",
     image: "https://covers.openlibrary.org/b/isbn/9781455586691-L.jpg",
-    affiliateLinkIN: "https://amzn.to/4i2IQxY",
-    affiliateLinkUS: "https://amzn.to/4s257es"
+    affiliateLinkIN: "https://amzn.to/4j2IQyY",
+    affiliateLinkUS: "https://amzn.to/4s2S7es"
   },
   {
     id: 4,
     title: "Rich Dad Poor Dad",
     author: "Robert T. Kiyosaki",
     image: "https://covers.openlibrary.org/b/isbn/9781612680194-L.jpg",
-    affiliateLinkIN: "https://amzn.to/3V01LAQ",
-    affiliateLinkUS: "https://amzn.to/4jCfpPx"
+    affiliateLinkIN: "https://amzn.to/3Y01LAQ",
+    affiliateLinkUS: "https://amzn.to/4j5cFPx"
   },
   {
     id: 5,
     title: "Wings of Fire",
     author: "A.P.J. Abdul Kalam",
     image: "https://covers.openlibrary.org/b/isbn/9788173711466-L.jpg",
-    affiliateLinkIN: "https://amzn.to/49eV9Dn",
-    affiliateLinkUS: "https://amzn.to/4sOQrRZI"
+    affiliateLinkIN: "https://amzn.to/49gV9En",
+    affiliateLinkUS: "https://amzn.to/4s0QRZj"
   },
   {
     id: 6,
     title: "The 4-Hour Workweek",
     author: "Tim Ferriss",
     image: "https://covers.openlibrary.org/b/isbn/9780307465351-L.jpg",
-    affiliateLinkIN: "https://amzn.to/3Li3BCM",
+    affiliateLinkIN: "https://amzn.to/3L618CM",
     affiliateLinkUS: "https://amzn.to/3Mzkdhg"
   },
   {
@@ -52,56 +52,56 @@ export const products = [
     title: "Ikigai",
     author: "Hector Garcia",
     image: "https://covers.openlibrary.org/b/isbn/9780143130727-L.jpg",
-    affiliateLinkIN: "https://amzn.to/3L7ylF4",
-    affiliateLinkUS: "https://amzn.to/4pjKeMU"
+    affiliateLinkIN: "https://amzn.to/3L7yRf4",
+    affiliateLinkUS: "https://amzn.to/4aTKxMU"
   },
   {
     id: 8,
     title: "Think and Grow Rich",
     author: "Napoleon Hill",
     image: "https://covers.openlibrary.org/b/isbn/9781585424337-L.jpg",
-    affiliateLinkIN: "https://amzn.to/4ic1no4",
-    affiliateLinkUS: "https://amzn.to/3Yua7lo"
+    affiliateLinkIN: "https://amzn.to/45c1oa4",
+    affiliateLinkUS: "https://amzn.to/3Yuo95s"
   },
   {
     id: 9,
     title: "The Alchemist",
     author: "Paulo Coelho",
     image: "https://covers.openlibrary.org/b/isbn/9780062315007-L.jpg",
-    affiliateLinkIN: "https://amzn.to/4gLtlX6",
-    affiliateLinkUS: "https://amzn.to/49cUSHf"
+    affiliateLinkIN: "https://amzn.to/4al3OXB",
+    affiliateLinkUS: "https://amzn.to/49eUSBP"
   },
   {
     id: 10,
     title: "Zero to One",
     author: "Peter Thiel",
     image: "https://covers.openlibrary.org/b/isbn/9780804139298-L.jpg",
-    affiliateLinkIN: "https://amzn.to/3p1K8xp",
-    affiliateLinkUS: "https://amzn.to/3jBbIEC"
+    affiliateLinkIN: "https://amzn.to/4p1K8vp",
+    affiliateLinkUS: "https://amzn.to/4j5bfEC"
   },
   {
     id: 11,
     title: "Eat That Frog!",
     author: "Brian Tracy",
     image: "https://covers.openlibrary.org/b/isbn/9781626569416-L.jpg",
-    affiliateLinkIN: "https://amzn.to/3plB8cn",
-    affiliateLinkUS: "https://amzn.to/3q7WxE4"
+    affiliateLinkIN: "https://amzn.to/4pJ8Bqn",
+    affiliateLinkUS: "https://amzn.to/4q7WeE4"
   },
   {
     id: 12,
     title: "Dopamine Detox",
     author: "Thibaut Meurisse",
     image: "https://covers.openlibrary.org/b/isbn/9788194724988-L.jpg",
-    affiliateLinkIN: "https://amzn.to/3V8CCX",
-    affiliateLinkUS: "https://amzn.to/3YxUjlH"
+    affiliateLinkIN: "https://amzn.to/4sl8CCX",
+    affiliateLinkUS: "https://amzn.to/3YxU3hl"
   },
   {
     id: 13,
     title: "I Will Teach You to Be Rich",
     author: "Ramit Sethi",
     image: "https://covers.openlibrary.org/b/isbn/9780761147480-L.jpg",
-    affiliateLinkIN: "https://amzn.to/4iXnsyo",
-    affiliateLinkUS: "https://amzn.to/4nIyxxq"
+    affiliateLinkIN: "https://amzn.to/4j0osye",
+    affiliateLinkUS: "https://amzn.to/45hyXHQ"
   },
   {
     id: 14,
@@ -109,39 +109,39 @@ export const products = [
     author: "Dale Carnegie",
     image: "https://covers.openlibrary.org/b/isbn/9780671733353-L.jpg",
     affiliateLinkIN: "https://amzn.to/3MKSha8",
-    affiliateLinkUS: "https://amzn.to/4pOn0Nl"
+    affiliateLinkUS: "https://amzn.to/4p0n0NI"
   },
   {
     id: 15,
     title: "The 5 AM Club",
     author: "Robin Sharma",
     image: "https://covers.openlibrary.org/b/isbn/9781443456623-L.jpg",
-    affiliateLinkIN: "https://amzn.to/4gOouVw",
-    affiliateLinkUS: "https://amzn.to/4q9SPum"
+    affiliateLinkIN: "https://amzn.to/4p0ouYw",
+    affiliateLinkUS: "https://amzn.to/4q9SPom"
   },
   {
     id: 16,
     title: "Coffee Can Investing",
     author: "Saurabh Mukherjea",
     image: "https://covers.openlibrary.org/b/isbn/9780670090457-L.jpg",
-    affiliateLinkIN: "https://amzn.to/4anmRUx",
-    affiliateLinkUS: "https://amzn.to/4geMKoc"
+    affiliateLinkIN: "https://amzn.to/4amKRUr",
+    affiliateLinkUS: "https://amzn.to/4qakKoc"
   },
   {
     id: 17,
     title: "12 Rules for Life",
     author: "Jordan Peterson",
     image: "https://covers.openlibrary.org/b/isbn/9780345816023-L.jpg",
-    affiliateLinkIN: "https://amzn.to/454vuZk",
-    affiliateLinkUS: "https://amzn.to/4z2SXJ1"
+    affiliateLinkIN: "https://amzn.to/454xvZk",
+    affiliateLinkUS: "https://amzn.to/4rZ5Xi1"
   },
   {
     id: 18,
     title: "The Power of Habit",
     author: "Charles Duhigg",
     image: "https://covers.openlibrary.org/b/isbn/9780812981605-L.jpg",
-    affiliateLinkIN: "https://amzn.to/44gBNl9",
-    affiliateLinkUS: "https://amzn.to/3KAR7xg"
+    affiliateLinkIN: "https://amzn.to/44zBNI9",
+    affiliateLinkUS: "https://amzn.to/3KAR7xq"
   },
   {
     id: 19,
@@ -149,15 +149,15 @@ export const products = [
     author: "Simon Sinek",
     image: "https://covers.openlibrary.org/b/isbn/9781591846444-L.jpg",
     affiliateLinkIN: "https://amzn.to/3MJ4rAr",
-    affiliateLinkUS: "https://amzn.to/3YA3Pl6"
+    affiliateLinkUS: "https://amzn.to/3YA3Pj6"
   },
   {
     id: 20,
     title: "The Compound Effect",
     author: "Darren Hardy",
     image: "https://covers.openlibrary.org/b/isbn/9781593157241-L.jpg",
-    affiliateLinkIN: "https://amzn.to/3ViIdXT",
-    affiliateLinkUS: "https://amzn.to/48U5Ig"
+    affiliateLinkIN: "https://amzn.to/3YlIdXT",
+    affiliateLinkUS: "https://amzn.to/48Ll5Ig"
   },
   {
     id: 21,
@@ -173,230 +173,230 @@ export const products = [
     author: "Benjamin Graham",
     image: "https://covers.openlibrary.org/b/isbn/9780060555665-L.jpg",
     affiliateLinkIN: "https://amzn.to/3L7MwCU",
-    affiliateLinkUS: "https://amzn.to/4p2OgLl"
+    affiliateLinkUS: "https://amzn.to/4p2OgLJ"
   },
   {
     id: 23,
     title: "The 7 Habits of Highly Effective People",
     author: "Stephen R. Covey",
     image: "https://covers.openlibrary.org/b/isbn/9780743269513-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+7+Habits+of+Highly+Effective+People+Stephen+R.+Covey",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+7+Habits+of+Highly+Effective+People+Stephen+R.+Covey"
+    affiliateLinkIN: "https://amzn.to/3YB6Pvw",
+    affiliateLinkUS: "https://amzn.to/4pN0OrE"
   },
   {
     id: 24,
     title: "Rework",
     author: "Jason Fried & David Heinemeier Hansson",
     image: "https://covers.openlibrary.org/b/isbn/9780307463746-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Rework+Jason+Fried",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Rework+Jason+Fried"
+    affiliateLinkIN: "https://amzn.to/4soNFXN",
+    affiliateLinkUS: "https://amzn.to/48Tu6O0"
   },
   {
     id: 25,
     title: "The Richest Man in Babylon",
     author: "George S. Clason",
     image: "https://covers.openlibrary.org/b/isbn/9780451205360-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+Richest+Man+in+Babylon+George+S.+Clason",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+Richest+Man+in+Babylon+George+S.+Clason"
+    affiliateLinkIN: "https://amzn.to/45aSVnD",
+    affiliateLinkUS: "https://amzn.to/4paXaH3"
   },
   {
     id: 26,
     title: "Steal Like an Artist",
     author: "Austin Kleon",
     image: "https://covers.openlibrary.org/b/isbn/9780761169253-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Steal+Like+an+Artist+Austin+Kleon",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Steal+Like+an+Artist+Austin+Kleon"
+    affiliateLinkIN: "https://amzn.to/498eM0d",
+    affiliateLinkUS: "https://amzn.to/4smzylI"
   },
   {
     id: 27,
     title: "Secrets of the Millionaire Mind",
     author: "T. Harv Eker",
     image: "https://covers.openlibrary.org/b/isbn/9780060763282-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Secrets+of+the+Millionaire+Mind+T.+Harv+Eker",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Secrets+of+the+Millionaire+Mind+T.+Harv+Eker"
+    affiliateLinkIN: "https://amzn.to/44DGQan",
+    affiliateLinkUS: "https://amzn.to/4rYfSnQ"
   },
   {
     id: 28,
     title: "Building a Second Brain",
     author: "Tiago Forte",
     image: "https://covers.openlibrary.org/b/isbn/9781982167387-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Building+a+Second+Brain+Tiago+Forte",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Building+a+Second+Brain+Tiago+Forte"
+    affiliateLinkIN: "https://amzn.to/493lK6u",
+    affiliateLinkUS: "https://amzn.to/3Y5679O"
   },
   {
     id: 29,
     title: "The Power of Your Subconscious Mind",
     author: "Joseph Murphy",
     image: "https://covers.openlibrary.org/b/isbn/9780553270114-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+Power+of+Your+Subconscious+Mind+Joseph+Murphy",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+Power+of+Your+Subconscious+Mind+Joseph+Murphy"
+    affiliateLinkIN: "https://amzn.to/4qpw7ZH",
+    affiliateLinkUS: "https://amzn.to/48QaSKw"
   },
   {
     id: 30,
     title: "Designing Your Life",
     author: "Bill Burnett & Dave Evans",
     image: "https://covers.openlibrary.org/b/isbn/9781101875322-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Designing+Your+Life+Bill+Burnett",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Designing+Your+Life+Bill+Burnett"
+    affiliateLinkIN: "https://amzn.to/4pH9UpS",
+    affiliateLinkUS: "https://amzn.to/44Dh4TK"
   },
   {
     id: 31,
     title: "Diamonds in the Dust",
     author: "Saurabh Mukherjea",
     image: "https://covers.openlibrary.org/b/isbn/9780670095308-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Diamonds+in+the+Dust+Saurabh+Mukherjea",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Diamonds+in+the+Dust+Saurabh+Mukherjea"
+    affiliateLinkIN: "https://amzn.to/3Le3FLf",
+    affiliateLinkUS: "https://amzn.to/4j31mag"
   },
   {
     id: 32,
     title: "Profit First",
     author: "Mike Michalowicz",
     image: "https://covers.openlibrary.org/b/isbn/9780735214149-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Profit+First+Mike+Michalowicz",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Profit+First+Mike+Michalowicz"
+    affiliateLinkIN: "https://amzn.to/498qOGV",
+    affiliateLinkUS: "https://amzn.to/4aXKoYP"
   },
   {
     id: 33,
     title: "The Millionaire Next Door",
     author: "Thomas J. Stanley",
     image: "https://covers.openlibrary.org/b/isbn/9781589795471-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+Millionaire+Next+Door+Thomas+J.+Stanley",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+Millionaire+Next+Door+Thomas+J.+Stanley"
+    affiliateLinkIN: "https://amzn.to/4aZ5qX2",
+    affiliateLinkUS: "https://amzn.to/3YD7BIt"
   },
   {
     id: 34,
     title: "Financial Freedom",
     author: "Grant Sabatier",
     image: "https://covers.openlibrary.org/b/isbn/9780525534594-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Financial+Freedom+Grant+Sabatier",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Financial+Freedom+Grant+Sabatier"
+    affiliateLinkIN: "https://amzn.to/3Y5mLGs",
+    affiliateLinkUS: "https://amzn.to/3L4WDZ7"
   },
   {
     id: 35,
     title: "The 80/20 Principle",
     author: "Richard Koch",
     image: "https://covers.openlibrary.org/b/isbn/9780385491747-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+80/20+Principle+Richard+Koch",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+80/20+Principle+Richard+Koch"
+    affiliateLinkIN: "https://amzn.to/3Na4OnC",
+    affiliateLinkUS: "https://amzn.to/456dnWM"
   },
   {
     id: 36,
     title: "Mindset",
     author: "Carol S. Dweck",
     image: "https://covers.openlibrary.org/b/isbn/9780345472328-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Mindset+Carol+S.+Dweck",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Mindset+Carol+S.+Dweck"
+    affiliateLinkIN: "https://amzn.to/44FebBT",
+    affiliateLinkUS: "https://amzn.to/4p3ugZi"
   },
   {
     id: 37,
     title: "Make Time",
     author: "Jake Knapp & John Zeratsky",
     image: "https://covers.openlibrary.org/b/isbn/9780525573425-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Make+Time+Jake+Knapp",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Make+Time+Jake+Knapp"
+    affiliateLinkIN: "https://amzn.to/4s5tVb9",
+    affiliateLinkUS: "https://amzn.to/4s3k31X"
   },
   {
     id: 38,
     title: "The Automatic Millionaire",
     author: "David Bach",
     image: "https://covers.openlibrary.org/b/isbn/9780767923828-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+Automatic+Millionaire+David+Bach",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+Automatic+Millionaire+David+Bach"
+    affiliateLinkIN: "https://amzn.to/4s1TFVX",
+    affiliateLinkUS: "https://amzn.to/4pTbjd6"
   },
   {
     id: 39,
     title: "The Power of Now",
     author: "Eckhart Tolle",
     image: "https://covers.openlibrary.org/b/isbn/9781577314806-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+Power+of+Now+Eckhart+Tolle",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+Power+of+Now+Eckhart+Tolle"
+    affiliateLinkIN: "https://amzn.to/4amYMK4",
+    affiliateLinkUS: "https://amzn.to/4s81xp0"
   },
   {
     id: 40,
     title: "Give and Take",
     author: "Adam Grant",
     image: "https://covers.openlibrary.org/b/isbn/9780143124986-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Give+and+Take+Adam+Grant",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Give+and+Take+Adam+Grant"
+    affiliateLinkIN: "https://amzn.to/3Y8lcaD",
+    affiliateLinkUS: "https://amzn.to/4999D8f"
   },
   {
     id: 41,
     title: "The Miracle Morning",
     author: "Hal Elrod",
     image: "https://covers.openlibrary.org/b/isbn/9780979019715-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+Miracle+Morning+Hal+Elrod",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+Miracle+Morning+Hal+Elrod"
+    affiliateLinkIN: "https://amzn.to/492AdzP",
+    affiliateLinkUS: "https://amzn.to/3L2Hw2j"
   },
   {
     id: 42,
     title: "Tiny Habits",
     author: "BJ Fogg",
     image: "https://covers.openlibrary.org/b/isbn/9780358003328-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Tiny+Habits+BJ+Fogg",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Tiny+Habits+BJ+Fogg"
+    affiliateLinkIN: "https://amzn.to/3Y5wwo0",
+    affiliateLinkUS: "https://amzn.to/4p6Ha91"
   },
   {
     id: 43,
     title: "Smarter Faster Better",
     author: "Charles Duhigg",
     image: "https://covers.openlibrary.org/b/isbn/9780812993394-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Smarter+Faster+Better+Charles+Duhigg",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Smarter+Faster+Better+Charles+Duhigg"
+    affiliateLinkIN: "https://amzn.to/4qjHfab",
+    affiliateLinkUS: "https://amzn.to/3LbFzAM"
   },
   {
     id: 44,
     title: "You Can",
     author: "George Matthew Adams",
     image: "https://covers.openlibrary.org/b/isbn/9789357025331-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=You+Can+George+Matthew+Adams",
-    affiliateLinkUS: "https://www.amazon.com/s?k=You+Can+George+Matthew+Adams"
+    affiliateLinkIN: "https://amzn.to/4pHalR2",
+    affiliateLinkUS: "https://amzn.to/4pOGGWk"
   },
   {
     id: 45,
     title: "The 5 Love Languages",
     author: "Gary Chapman",
     image: "https://covers.openlibrary.org/b/isbn/9780802412706-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+5+Love+Languages+Gary+Chapman",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+5+Love+Languages+Gary+Chapman"
+    affiliateLinkIN: "https://amzn.to/4qpZuuZ",
+    affiliateLinkUS: "https://amzn.to/4s2NGAp"
   },
   {
     id: 46,
     title: "Do It Today",
     author: "Darius Foroux",
     image: "https://covers.openlibrary.org/b/isbn/9780143452126-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Do+It+Today+Darius+Foroux",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Do+It+Today+Darius+Foroux"
+    affiliateLinkIN: "https://amzn.to/4rYgkm2",
+    affiliateLinkUS: "https://amzn.to/45dagML"
   },
   {
     id: 47,
     title: "Better Than Before",
     author: "Gretchen Rubin",
     image: "https://covers.openlibrary.org/b/isbn/9780385348614-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Better+Than+Before+Gretchen+Rubin",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Better+Than+Before+Gretchen+Rubin"
+    affiliateLinkIN: "https://amzn.to/4qpw7ZH",
+    affiliateLinkUS: "https://amzn.to/48QaSKw"
   },
   {
     id: 48,
     title: "The Buffett Way",
     author: "Robert G. Hagstrom",
     image: "https://covers.openlibrary.org/b/isbn/9781118793992-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+Buffett+Way+Robert+G.+Hagstrom",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+Buffett+Way+Robert+G.+Hagstrom"
+    affiliateLinkIN: "https://amzn.to/3KMYBxl",
+    affiliateLinkUS: "https://amzn.to/3XZovRn"
   },
   {
     id: 49,
     title: "The Art of Spending Money",
     author: "Morgan Housel",
     image: "https://covers.openlibrary.org/b/isbn/9781804091890-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=The+Art+of+Spending+Money+Morgan+Housel",
-    affiliateLinkUS: "https://www.amazon.com/s?k=The+Art+of+Spending+Money+Morgan+Housel"
+    affiliateLinkIN: "https://amzn.to/3Y5q6Fi",
+    affiliateLinkUS: "https://amzn.to/4qhwipH"
   },
   {
     id: 50,
     title: "Rich Routines",
     author: "Steve Houghton",
     image: "https://covers.openlibrary.org/b/isbn/9798991017800-L.jpg",
-    affiliateLinkIN: "https://www.amazon.in/s?k=Rich+Routines+Steve+Houghton",
-    affiliateLinkUS: "https://www.amazon.com/s?k=Rich+Routines+Steve+Houghton"
+    affiliateLinkIN: "https://amzn.to/48YCw6R",
+    affiliateLinkUS: "https://amzn.to/3KEopf6"
   }
 ];

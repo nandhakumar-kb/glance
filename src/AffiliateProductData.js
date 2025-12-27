dcc5d0fa84fe0445068bbd2bdf8c645afeabda37
nandhakumar-kb <nandhakumarkb2005@gmail.com>
2025-12-27 16:15:20 +0530

    pro

--- src/AffiliateProductData.js
+++ src/AffiliateProductData.js
@@ -1,7 +1,6 @@
 export const affiliateProducts = [
-
   {
-    id: 2,
+    id: 1,
     title: "Nivea Men Creme, Dark Spot Reduction, Non Greasy Moisturizer, Cream With Uv Protect, 150 ml",
     description: "The Nivea Dark Spot Reduction Crème is specifically formulated for men's skin and is a daily use moisturizer",
     image: "https://m.media-amazon.com/images/I/51Jer-OTUtL._SX679_.jpg",
